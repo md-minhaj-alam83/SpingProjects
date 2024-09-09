4cc2320fd35131dcdeca1ad6b077b92ff6fa6caf
Md Minhaj Alam <94886118+md-minhaj-alam83@users.noreply.github.com>
2024-09-09 21:22:37 +0530

    Add files via upload
    setter method dependency with java file.

---
+++ 7.SetterMethodDIwithJavaFileProject/src/in/min/resources/SpringConfigFile.java
@@ -0,0 +1,34 @@
+package in.min.resources;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import in.min.beans.Address;
+import in.min.beans.Student;
+
+@Configuration
+public class SpringConfigFile {
+	
+	@Bean
+	public Address createAddrObj() {
+		
+		Address addr = new Address();
+		addr.setHouseNo(137);
+		addr.setCity("Purnia");
+		addr.setPinCode(854326);
+		
+		return addr;
+	} 
+	
+	@Bean
+	public Student createStdObj() {
+		
+		Student std = new Student();
+		std.setName("Minhaj");
+		std.setRollNo(101);
+		std.setAddress(createAddrObj());
+		
+		return std;
+	}
+
+}
